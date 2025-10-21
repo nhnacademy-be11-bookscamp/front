d0628c48e703e1eec68a41f78c73640cb0c51d33
Kiju Jeong <kijujeong.dev@gmail.com>
2025-10-21 11:22:43 +0900

    init

---
+++ src/main/java/store/bookscamp/front/ViewController.java
@@ -0,0 +1,18 @@
+package store.bookscamp.front;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "layout";
+    }
+
+    @GetMapping("/template-no-sidebar")
+    public String templateNoSidebar() {
+        return "template-no-siderbar";
+    }
+}

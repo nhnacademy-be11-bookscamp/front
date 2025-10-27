0360cf4775498229bb3bde4eddc04aaa5c3121d8
BaekSungJun <por9535@gmail.com>
2025-10-27 15:43:37 +0900

    feat:머지후 중복된 어노테이션 삭제

--- src/main/java/store/bookscamp/front/FrontApplication.java
+++ src/main/java/store/bookscamp/front/FrontApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-@EnableFeignClients
 public class FrontApplication {
 
 	public static void main(String[] args) {

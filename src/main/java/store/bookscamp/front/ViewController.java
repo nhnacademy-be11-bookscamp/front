a95047884622a1892dc4f81b8272592eb1cc0ffa
김가현 <kimgh@gimgahyeon-ui-MacBookAir.local>
2025-10-27 13:05:05 +0900

    feat: tag view

--- src/main/java/store/bookscamp/front/ViewController.java
+++ src/main/java/store/bookscamp/front/ViewController.java
@@ -2,6 +2,7 @@ package store.bookscamp.front;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
@@ -9,5 +10,10 @@ public class ViewController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/tag")
+    public String tag() {
+        return "tag";
     }
 }

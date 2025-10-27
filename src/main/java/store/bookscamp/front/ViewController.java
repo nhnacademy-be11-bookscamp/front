6bec56e2b9b2568d239d4bff3a48593c4c93d8a8
Yoon-Bin99 <shp06135@naver.com>
2025-10-27 17:59:23 +0900

    카테고리 클릭시 도서 조회 및 정렬 기능 (#6)
    * 도서 조회
    
    * 도서 조회 기능
    
    * html 기본 틀에 맞춰 수정

--- src/main/java/store/bookscamp/front/ViewController.java
+++ src/main/java/store/bookscamp/front/ViewController.java
@@ -1,13 +1,30 @@
 package store.bookscamp.front;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import store.bookscamp.front.category.dto.CategoryListResponse;
+import store.bookscamp.front.category.feign.CategoryFeignClient;
 
 @Controller
+@RequiredArgsConstructor
 public class ViewController {
 
+    private final CategoryFeignClient categoryFeignClient;
+
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        try {
+            List<CategoryListResponse> categories = categoryFeignClient.getAllCategories();
+            model.addAttribute("categories", categories);
+
+        } catch (Exception e) {
+            model.addAttribute("categories", List.of()); // 빈 리스트 전달
+            System.err.println("API 호출 실패: " + e.getMessage());
+        }
         return "index";
     }
 

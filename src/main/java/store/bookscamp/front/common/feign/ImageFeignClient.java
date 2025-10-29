172ece1d32dbb773fbb9a0e8e8ed0dbaed5b60cd
cherrrryk <kimcherry01@naver.com>
2025-10-29 16:43:11 +0900

    feat:imageFeignClient 생성, 삭제

--- src/main/java/store/bookscamp/front/common/feign/ImageFeignClient.java
+++ src/main/java/store/bookscamp/front/common/feign/ImageFeignClient.java
@@ -3,7 +3,7 @@ package store.bookscamp.front.common.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,9 +19,16 @@ import java.util.List;
 public interface ImageFeignClient {
 
     @PostMapping(
-            value = "/api/{type}", // api서버 컨트롤러 쪽 api랑 맞추기
+            value = "/admin/book/image", // api서버 컨트롤러 api랑 맞추기
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE
     )
-    ResponseEntity<List<String>> uploadBookImages(@PathVariable("type") String type,
-                                                  @RequestPart("files") List<MultipartFile> files);
+        // api서버 컨트롤러 response랑 맞추기 -> List<String> 바꾸기
+    ResponseEntity<List<String>> creatBookImage(@RequestPart("files") List<MultipartFile> files);
+
+    @DeleteMapping(
+            value = "/admin/book/image", // api서버 컨트롤러 쪽 api랑 맞추기
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+        // api서버 컨트롤러 request랑 맞추기
+    ResponseEntity<Void> deleteBookImage();
 }

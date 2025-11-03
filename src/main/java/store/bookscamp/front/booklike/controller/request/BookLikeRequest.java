b4c6c62cf077c62f74d67da2eb3c08121179bd76
Yoon-Bin99 <shp06135@naver.com>
2025-11-03 20:50:01 +0900

    좋아요 기능 구현

---
+++ src/main/java/store/bookscamp/front/booklike/controller/request/BookLikeRequest.java
@@ -0,0 +1,7 @@
+package store.bookscamp.front.booklike.controller.request;
+
+public record BookLikeRequest(
+
+        boolean liked
+) {
+}
--- src/main/java/store/bookscamp/front/category/controller/CategoryController.java
+++ src/main/java/store/bookscamp/front/category/controller/CategoryController.java
@@ -4,11 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import store.bookscamp.front.category.feign.CategoryFeignClient;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,6 +18,6 @@ public class CategoryController {
     @GetMapping
     public String category(Model model){
         model.addAttribute("apiPrefix", pathPrefix);
-        return "category/category";
+        return "admin/category";
     }
 }

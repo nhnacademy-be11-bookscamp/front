967d26636c1031303a3fb79f8a922fef351b6c5a
Kiju Jeong <kijujeong.dev@gmail.com>
2025-11-06 16:33:25 +0900

    feat: 자잘한 오류 수정2

--- src/main/java/store/bookscamp/front/common/config/WebConfig.java
+++ src/main/java/store/bookscamp/front/common/config/WebConfig.java
@@ -3,7 +3,6 @@ package store.bookscamp.front.common.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @Configuration

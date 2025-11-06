15bc6dddbe5d42468ef36609d7f3020b323fc40e
kiju-dev <kijujeong.dev@gmail.com>
2025-11-06 16:34:44 +0900

    Feature/front refactoring all (#44)
    * feat: 임시 커밋
    
    * feat: 자잘한 오류 수정
    
    * feat: 자잘한 오류 수정2

--- src/main/java/store/bookscamp/front/book/controller/response/AladinBookResponse.java
+++ src/main/java/store/bookscamp/front/book/controller/response/AladinBookResponse.java
@@ -2,6 +2,7 @@ package store.bookscamp.front.book.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
 import java.util.List;
 
 @Data
--- src/main/java/store/bookscamp/front/member/controller/MemberFeignClient.java
+++ src/main/java/store/bookscamp/front/member/controller/MemberFeignClient.java
@@ -4,14 +4,11 @@ package store.bookscamp.front.member.controller;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import store.bookscamp.front.member.controller.request.MemberCreateRequest;
 import store.bookscamp.front.member.controller.request.MemberPasswordUpdateRequest;
 import store.bookscamp.front.member.controller.request.MemberUpdateRequest;

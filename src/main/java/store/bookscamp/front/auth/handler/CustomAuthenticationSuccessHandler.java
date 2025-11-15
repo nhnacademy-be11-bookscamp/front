0e5604ebb217ff6c64167ad5df7819e5d5e1a8a0
SungJunBaek <63231250+BaekSeongJun@users.noreply.github.com>
2025-11-15 21:35:53 +0900

    [Feature/인증] 쿠키설정변경 (#122)
    * feat:userName -> username
    
    * feat:local-> 배포용 url 주소
    
    * feat:ajax 설정 추가
    
    * feat:기본 yml 설정
    
    * feat:url설정
    
    * 임시저장
    
    * fear:잘못된 mapping 수정
    
    * fear:url 수정
    
    * feat: 로그인 성공시 jwt발급 및 회원쪽 로직 수정
    
    * feat: 회원가입 회원수정 로그인 로직 front 수정
    
    * feat: 경로확인
    
    * feat: 경로 확인
    
    * feat: 찐막
    
    * feat: 403 권한 없는 접근에 대한 예외 처리 + 안쓰는 import문 삭제
    
    * feat: 토큰 관리 session -> cookie를 들고와 gateway에서 검증
    
    * feat: 토큰 관리 session -> cookie를 들고와 gateway에서 검증
    
    * ?
    
    * feat : refresh토큰
    
    * feat : refresh토큰
    
    * feat : mypage 프론트에 연결
    
    * feat : icon
    
    * feat: member-name-handler.js 추가
    
    * Feature/address front (#120)
    
    * Address Feign Client
    
    * html 추가
    
    * address front 구현
    
    * 최종
    
    * html 삭제
    
    * 주석해제
    
    * feat:address front html 추가
    
    * feat:logback 최종
    
    * feat: logback
    
    * feat: logback
    
    * fix: url 수정 및 mypage.html 수정
    
    * fix: logback 압축 제거
    
    * feat: mypage-address front 구현
    
    * fix: mapping 수정
    
    * fix: log 파일 제거
    
    * fix: /제거
    
    * feat : cookie 설정 변경 및 ajax 통신 방향을 front가 향하도록 변경
    
    ---------
    
    Co-authored-by: Kiju Jeong <kijujeong.dev@gmail.com>
    Co-authored-by: dldnjswnd <aricatao@naver.com>

--- src/main/java/store/bookscamp/front/auth/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/store/bookscamp/front/auth/handler/CustomAuthenticationSuccessHandler.java
@@ -1,11 +1,12 @@
 package store.bookscamp.front.auth.handler;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLEncoder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -26,32 +27,42 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 
         LoginAuthDetails loginDetails = (LoginAuthDetails) authentication.getDetails();
 
+        boolean isSecure = request.isSecure();
+
+        if (request.getHeader("x-forwarded-proto") != null) {
+            isSecure = request.getHeader("x-forwarded-proto").equals("https");
+        }
+
         String rawJwtToken = loginDetails.getRawAccessToken();
         if (rawJwtToken != null && rawJwtToken.startsWith("Bearer ")) {
             String jwtToken = rawJwtToken.substring(7);
 
-            Cookie cookie = new Cookie("Authorization", jwtToken);
-            cookie.setPath("/");
-            cookie.setHttpOnly(true);
-            response.addCookie(cookie);
+            ResponseCookie atCookie = ResponseCookie.from("Authorization", jwtToken)
+                    .path("/")
+                    .httpOnly(true)
+                    .secure(isSecure)
+                    .sameSite(isSecure ? "None" : "Lax")
+                    .build();
+            response.addHeader(HttpHeaders.SET_COOKIE, atCookie.toString());
         }
 
         String rtCookieString = loginDetails.getRtCookieString();
         if (rtCookieString != null) {
-            response.addHeader("Set-Cookie", rtCookieString);
+            response.addHeader(HttpHeaders.SET_COOKIE, rtCookieString);
         }
 
         String name = loginDetails.getName();
         if (name != null) {
-            Cookie nameCookie = new Cookie("member_name", URLEncoder.encode(name, "UTF-8"));
-            nameCookie.setPath("/");
-            nameCookie.setHttpOnly(false);
-            nameCookie.setMaxAge(60 * 30);
-
-            response.addCookie(nameCookie);
+            ResponseCookie nameCookie = ResponseCookie.from("member_name", URLEncoder.encode(name, "UTF-8"))
+                    .path("/")
+                    .httpOnly(false)
+                    .secure(isSecure)
+                    .sameSite(isSecure ? "None" : "Lax")
+                    .maxAge(60 * 30)
+                    .build();
+            response.addHeader(HttpHeaders.SET_COOKIE, nameCookie.toString());
         }
 
         this.delegate.onAuthenticationSuccess(request, response, authentication);
     }
-
 }

76f747b580022f548e71cd407243569597f70658
eahc00 <eahc00@naver.com>
2025-11-17 14:28:51 +0900

    feat: 포인트 정책 기능 구현

---
+++ src/main/java/store/bookscamp/front/pointpolicy/controller/enums/PointPolicyType.java
@@ -0,0 +1,12 @@
+package store.bookscamp.front.pointpolicy.controller.enums;
+
+public enum PointPolicyType {
+
+    ALL,
+    STANDARD,
+    ROYAL,
+    GOLD,
+    PLATINUM,
+    REVIEW,
+    WELCOME
+}

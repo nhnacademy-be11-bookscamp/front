d7c848836229712ff29028d4a920f96a155fde57
BaekSungJun <por9535@gmail.com>
2025-11-18 14:12:06 +0900

    feat : Oauth2 session클러스터링

--- src/main/java/store/bookscamp/front/FrontApplication.java
+++ src/main/java/store/bookscamp/front/FrontApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import store.bookscamp.front.common.config.FeignConfig;
 
 @EnableCaching
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRedisHttpSession
 @EnableFeignClients(defaultConfiguration = FeignConfig.class)
 public class FrontApplication {
 

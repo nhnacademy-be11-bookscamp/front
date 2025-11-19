68af5147cb34680af4747e262d75c78e3b318c68
BaekSungJun <por9535@gmail.com>
2025-11-19 17:06:32 +0900

    feat : 회원 dooray인증 로직 추가

--- src/main/java/store/bookscamp/front/auth/repository/AuthFeignClient.java
+++ src/main/java/store/bookscamp/front/auth/repository/AuthFeignClient.java
@@ -1,6 +1,7 @@
-package store.bookscamp.front.admin.repository;
+package store.bookscamp.front.auth.repository;
 
 import jakarta.validation.Valid;
+import java.util.Map;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -10,13 +11,10 @@ import store.bookscamp.front.admin.controller.request.AdminLoginRequest;
 import store.bookscamp.front.auth.dto.AccessTokenResponse;
 import store.bookscamp.front.auth.dto.OauthLoginRequest;
 import store.bookscamp.front.common.config.AuthFeignConfig;
+import store.bookscamp.front.member.controller.request.MemberLoginRequest;
 
-@FeignClient(name = "adminLoginClient", url = "${gateway.base-url}", configuration = AuthFeignConfig.class)
-public interface AdminLoginFeignClient {
-
-    @PostMapping("/auth-server/admin/login")
-    public ResponseEntity<AccessTokenResponse> doLogin(@Valid @RequestBody AdminLoginRequest adminLoginRequest);
-
+@FeignClient(name = "authFeignClient", url = "${gateway.base-url}", configuration = AuthFeignConfig.class)
+public interface AuthFeignClient {
     @PostMapping("/auth-server/oauth/login")
     ResponseEntity<AccessTokenResponse> oauthLogin(@RequestBody OauthLoginRequest request);
 
@@ -25,6 +23,16 @@ public interface AdminLoginFeignClient {
 
     @PostMapping("/auth-server/logout")
     ResponseEntity<Void> doLogout(@CookieValue("refresh_token") String refreshToken);
+
+    @PostMapping("/auth-server/login")
+    ResponseEntity<AccessTokenResponse> doLogin(@Valid @RequestBody MemberLoginRequest memberLoginRequest);
+
+    @PostMapping("/auth-server/admin/login")
+    ResponseEntity<AccessTokenResponse> doLogin(@Valid @RequestBody AdminLoginRequest adminLoginRequest);
+
+    @PostMapping("/auth-server/dormant/send")
+    ResponseEntity<Map<String, String>> sendDormantCode(@RequestBody Map<String, String> request);
+
+    @PostMapping("/auth-server/dormant/verify")
+    ResponseEntity<Map<String, String>> verifyDormantCode(@RequestBody Map<String, String> request);
 }
-
-

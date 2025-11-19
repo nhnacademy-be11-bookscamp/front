5f646fb417bc632ae51c7d4e237f7861196dca40
김가현 <kimgh@gimgahyeon-ui-MacBookAir.local>
2025-11-19 14:34:34 +0900

    fix: deliveryPolicy create() 최초배송비정책 추가하도록 수정

--- src/main/java/store/bookscamp/front/deliverypolicy/controller/DeliveryPolicyController.java
+++ src/main/java/store/bookscamp/front/deliverypolicy/controller/DeliveryPolicyController.java
@@ -20,20 +20,6 @@ public class DeliveryPolicyController {
 
     private final DeliveryPolicyService service;
 
-    /*
-    @GetMapping("/admin/delivery-policy")
-    public String getAdminDeliveryPolicy(Model model) {
-        model.addAttribute("policy", service.getDeliveryPolicy());
-        return "admin/delivery-policy";
-    }
-
-    @PostMapping("/admin/delivery-policy") // PutMapping
-    public String updateDeliveryPolicy(@Valid DeliveryPolicyUpdateRequest request) {
-        service.updateDeliveryPolicy(request);
-        return "redirect:/admin/delivery-policy";
-    }
-     */
-
     @GetMapping("/admin/delivery-policy")
     public String getDeliveryPolicy(Model model) {
         try {
@@ -88,18 +74,4 @@ public class DeliveryPolicyController {
         return "redirect:/admin/delivery-policy";
     }
 
-//    @PostMapping("/admin/delivery-policy")
-//    public String updateDeliveryPolicy(
-//            @Valid @ModelAttribute("form") DeliveryPolicyUpdateRequest form,
-//            BindingResult bindingResult,
-//            Model model,
-//            RedirectAttributes ra) {
-//        if (bindingResult.hasErrors()) {
-//            model.addAttribute("policy", service.getDeliveryPolicy());
-//            return "admin/delivery-policy";
-//        }
-//        service.updateDeliveryPolicy(form);
-//        ra.addFlashAttribute("successMessage", "저장되었습니다.");
-//        return "redirect:/admin/delivery-policy";
-//    }
 }

cfe6a6b75539dd4e39af6fc6974c7516c1404025
BaekSungJun <por9535@gmail.com>
2025-12-03 19:51:04 +0900

    feat : 로직 일부 수정

--- src/main/java/store/bookscamp/front/common/exception/GlobalExceptionHandler.java
+++ src/main/java/store/bookscamp/front/common/exception/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package store.bookscamp.front.common.exception;
 
 import feign.FeignException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -18,5 +22,12 @@ public class GlobalExceptionHandler {
         model.addAttribute("errorMessage", errorMessage);
 
         return "error/error";
+    }
+
+    @ExceptionHandler(RefreshTokenExpiredException.class)
+    public String handleTokenExpired(RefreshTokenExpiredException ex) {
+        log.warn("리프레시 토큰 만료 -> 스프링 시큐리티 로그아웃 체인으로 리다이렉트");
+
+        return "redirect:/logout";
     }
 }
